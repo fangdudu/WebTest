4b02510bdf42e5398917a02425891c91c0c1a518
fangdudu <1042096700@qq.com>
2020-03-01 23:01:33 +0800

    修改

--- style/scripts/h5-test.js
+++ style/scripts/h5-test.js
@@ -2,7 +2,7 @@ var x = document.getElementById("loc");
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition,showError);
     } else {
         x.innerHTML = "该游览器不支持获取地理位置";
     }
@@ -10,4 +10,23 @@ function getLocation() {
 
 function showPosition(position) {
     x.innerHTML = "纬度：" + position.coords.latitude + "<br> 经度：" + position.coords.longitude;
+}
+
+function showError(error)
+{
+    switch(error.code) 
+    {
+        case error.PERMISSION_DENIED:
+            x.innerHTML="用户拒绝对获取地理位置的请求。"
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML="位置信息是不可用的。"
+            break;
+        case error.TIMEOUT:
+            x.innerHTML="请求用户地理位置超时。"
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML="未知错误。"
+            break;
+    }
 }
